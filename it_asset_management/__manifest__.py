cc6dacd9d97851086958da8e7184037cb11ae547
Jonah Harivelona <jonahrakoto2001@gmail.com>
2026-09-18 18:33:19 +0200

    Add it_asset_management module

---
+++ it_asset_management/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "IT Asset Management",  
+    "version": "1.0",
+    'description':'IT Asset & Maintenance Management',
+    'depends': ['maintenance'],
+    'data':[
+        
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+    'author': 'Jonah',
+}
